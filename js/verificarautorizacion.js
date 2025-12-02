664d8f6100373d00ba8fc3121b52cf557ea3122e
lacabannarese <lacabannarese@gmail.com>
2025-12-02 14:48:03 +0000

    Delete js directory

--- js/verificarautorizacion.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const rawSession = localStorage.getItem('userSession');
-    const session = rawSession ? JSON.parse(rawSession) : {};
-    const tipo = session.tipo || "usuario";
-
-    if (tipo === "usuario") {
-        window.location.href = "index.html";
-    }
-});
